42fa755910c140baf9aadcbae76b270c2ff2be5c
Manuel Kessel <liproqq@gmail.com>
2021-05-06 09:47:17 +0200

    margins

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import './App.css';
 const csvUrl = 'https://gist.githubusercontent.com/curran/0ac4077c7fc6390f5dd33bf5c06cb5ff/raw/605c54080c7a93a417a3cea93fd52e7550e76500/UN_Population_2019.csv'
 const width = 960
 const height = 500
+const margin = { top: 20, right: 20, bottom: 20, left: 20 };
+const innerHeight = height - margin.top - margin.bottom
+const innerWidth = width - margin.left - margin.right
 
 
 const App = () => {
@@ -25,19 +28,21 @@ const App = () => {
 
   const yScale = scaleBand()
     .domain(data.map(d => d.Country))
-    .range([0, height])
+    .range([0, innerHeight])
 
   const xScale = scaleLinear()
     .domain([0, max(data, d => d.Population)])
-    .range([0, width])
+    .range([0, innerWidth])
 
   return (
     <svg width={width} height={height}>
-      {data.map(d => <rect
-        x={0}
-        y={yScale(d.Country)}
-        width={xScale(d.Population)}
-        height={yScale.bandwidth()} />)}
+      <g transform={`translate(${margin.left}, ${margin.right})`}>
+        {data.map(d => <rect
+          x={0}
+          y={yScale(d.Country)}
+          width={xScale(d.Population)}
+          height={yScale.bandwidth()} />)}
+      </g>
     </svg>
   )
 };
