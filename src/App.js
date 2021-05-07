b7d3629f1da76c7f37392f78768771209768ff96
Manuel Kessel <liproqq@gmail.com>
2021-05-07 15:17:14 +0200

    added menu to scatterplot

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import { useState } from 'react'
 import { scaleLinear, extent, format } from 'd3';
 import './App.css';
 import { AxisBottom } from './AxisBottom';
 import { AxisLeft } from './AxisLeft';
+import { Dropdown } from './Dropdown';
 import { Marks } from './Marks';
 import { useData } from './useData';
 
@@ -11,20 +13,35 @@ const margin = { top: 20, right: 30, bottom: 70, left: 100 };
 const innerHeight = height - margin.top - margin.bottom
 const innerWidth = width - margin.left - margin.right
 
+const attributes = [
+  { value: 'sepal_length', label: 'Sepal Length' },
+  { value: 'sepal_width', label: 'Sepal Width' },
+  { value: 'petal_length', label: 'Petal Length' },
+  { value: 'petal_width', label: 'Petal Width' },
+  { value: 'species', label: 'Species' }
+];
+
+const getLabel = value => attributes.find(att => att.value === value).label
+
 const App = () => {
+  const initialXAttribute = 'sepal_length'
+  const initialYAttribute = 'sepal_width'
   const data = useData()
+  const [xAttribute, setXAttribute] = useState(initialXAttribute)
+  const [yAttribute, setYAttribute] = useState(initialYAttribute)
 
   if (!data) {
     return <pre>Loading</pre>
   }
 
-  const xValue = d => d.sepal_length
-  const xAxisLabel = 'Sepal Length'
-  const xAxisLabelOffset = 50;
 
-  const yValue = d => d.sepal_width
-  const yAxisLabel = 'Sepal Width'
+  const xValue = d => d[xAttribute]
+  const xAxisLabel = getLabel(xAttribute)
+  const xAxisLabelOffset = 50;
+  const yValue = d => d[yAttribute]
+  const yAxisLabel = getLabel(yAttribute)
   const yAxisLabelOffset = 50;
+
 
   const xScale = scaleLinear()
     .domain(extent(data, xValue))
@@ -36,45 +53,61 @@ const App = () => {
     .range([0, innerHeight])
 
   return (
-    <svg width={width} height={height}>
-      <g transform={`translate(${margin.left}, ${margin.right})`}>
-        <AxisBottom
-          xScale={xScale}
-          innerHeight={innerHeight}
-          tickFormat={format(".2s")} />
-        <AxisLeft
-          innerWidth={innerWidth}
-          yScale={yScale}
-        />
-        <text
-          className="axis-label"
-          textAnchor="middle"
-          x={innerWidth / 2}
-          y={innerHeight + xAxisLabelOffset}
-        >
-          {xAxisLabel}
-        </text>
-        <text
-          className="axis-label"
-          textAnchor="middle"
-          transform={
-            `translate(
+    <>
+      <Dropdown
+        options={attributes}
+        id="x-select"
+        selectedValue={xAttribute}
+        onSelectedValueChange={setXAttribute}
+        labelText="X:"
+      />
+      <Dropdown
+        options={attributes}
+        id="y-select"
+        selectedValue={yAttribute}
+        onSelectedValueChange={setYAttribute}
+        labelText="Y:"
+      />
+      <svg width={width} height={height}>
+        <g transform={`translate(${margin.left}, ${margin.right})`}>
+          <AxisBottom
+            xScale={xScale}
+            innerHeight={innerHeight}
+            tickFormat={format(".2s")} />
+          <AxisLeft
+            innerWidth={innerWidth}
+            yScale={yScale}
+          />
+          <text
+            className="axis-label"
+            textAnchor="middle"
+            x={innerWidth / 2}
+            y={innerHeight + xAxisLabelOffset}
+          >
+            {xAxisLabel}
+          </text>
+          <text
+            className="axis-label"
+            textAnchor="middle"
+            transform={
+              `translate(
             ${-yAxisLabelOffset},
             ${innerHeight / 2}) 
             rotate(-90)`
-          }
-        >
-          {yAxisLabel}
-        </text>
-        <Marks
-          data={data}
-          xScale={xScale}
-          yScale={yScale}
-          xValue={xValue}
-          yValue={yValue}
-        />
-      </g>
-    </svg>
+            }
+          >
+            {yAxisLabel}
+          </text>
+          <Marks
+            data={data}
+            xScale={xScale}
+            yScale={yScale}
+            xValue={xValue}
+            yValue={yValue}
+          />
+        </g>
+      </svg>
+    </>
   )
 };
 
